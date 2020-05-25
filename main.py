e9a53cc90dba48cb719d3c7328f36c3a8047099c
Nathan MacLeod <nathan.a.macleod@gmail.com>
2020-05-25 22:52:27 +0100

    Completely revamped the system..
    You can now specify the location of vertices and draw them easily

--- main.py
+++ main.py
@@ -1,54 +1,34 @@
-screen_width = 50 # The amount of characters accross the screen
-screen_height = 25
-background_char = "." # Character to use when printing the background
-vertex_char = "V" # Character to use when printing a vertex
-canvas_char = "#" # Character to use for the edges of the canvas
+# Main classes:
+class scene:
+	def __init__(self, canvWidth, canvHeight):
+		self.canvWidth = canvWidth
+		self.canvHeight = canvHeight
+		self.canvas = []
 
-def clearCanvas():
-    for i in range(0, screen_width):
-        print(canvas_char, end="")
+	# Create the array for the canvas
+	def resetCanvas(self):
+		self.canvas = []
+		for y in range(0, self.canvHeight):
+			currentLine = []
+			for x in range(0, self.canvWidth):
+				currentLine.append("`") # "`" is the background character
 
-    print() # Print a new line
+			self.canvas.append(currentLine)
 
-    for i in range(0, screen_height):
-        for i in range(0, screen_width):
-            print(".", end="")
+	def printCanvas(self):
+		# Actually display (print) the canvas
+		for i in range(0, len(self.canvas)):
+			for y in range(0, len(self.canvas[i])):
+				print(self.canvas[i][y], end="")
+					
+			print()
 
-        print() # Print a new line
+	def addVertex(self, vertX, vertY):
+		self.canvas[vertY][vertX] = "#" # "/" is the character representing a vertex
 
-    for i in range(0, screen_width):
-        print(canvas_char, end="")
-
-    print() # Print a new line
-
-class vertex:
-    def __init__(self, xCoord, yCoord, zCoord):
-        self.xCoord = xCoord;
-        self.yCoord = yCoord;
-        self.zCoord = zCoord;
-
-    def displayVertices(self):
-        for i in range(0, screen_height):
-            if i != self.yCoord:
-                for c in range(0, screen_width):
-                    print(background_char, end="")
-
-            else:
-                for x in range(0, screen_width):
-                    if x != self.xCoord:
-                        print(background_char, end="")
-
-                    else:
-                        print(vertex_char, end="")
-
-            print() # Print a new line
-
-class edge:
-    def __init__(self, vertex1, vertex2):
-        self.vertex1 = vertex1;
-        self.vertex2 = vertex2;
-
-clearCanvas()
-
-vert1 = vertex(20, 3, 15)
-vert1.displayVertices()
+# The rest if the code just makes use of the classes
+mainScene = scene(30, 15)
+mainScene.resetCanvas()
+mainScene.addVertex(3, 2)
+mainScene.addVertex(5, 9)
+mainScene.printCanvas()
